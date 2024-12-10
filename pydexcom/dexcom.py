75ce72bac5fc71d7f1554df78b60533d1f8032a9
gagebenne <gagedbenne@gmail.com>
2024-12-09 21:17:47 -0500

    Separate out common items

--- pydexcom/dexcom.py
+++ pydexcom/dexcom.py
@@ -13,6 +13,7 @@ from .const import (
     DEXCOM_BASE_URLS,
     DEXCOM_GLUCOSE_READINGS_ENDPOINT,
     DEXCOM_LOGIN_ID_ENDPOINT,
+    HEADERS,
     MAX_MAX_COUNT,
     MAX_MINUTES,
     Region,
@@ -48,11 +49,7 @@ class Dexcom:
         :param password: password for the Dexcom Share user.
         :param region: the region to use, one of `"us"`, `"ous"`, `"jp"`.
         """
-        user_ids = sum(user_id is not None for user_id in [account_id, username])
-        if user_ids == 0:
-            raise ArgumentError(ArgumentErrorEnum.NONE_USER_ID_PROVIDED)
-        if user_ids != 1:
-            raise ArgumentError(ArgumentErrorEnum.TOO_MANY_USER_ID_PROVIDED)
+        self._validate_user_ids(username, account_id)
 
         self._base_url = DEXCOM_BASE_URLS[region]
         self._application_id = DEXCOM_APPLICATION_IDS[region]
@@ -60,8 +57,8 @@ class Dexcom:
         self._username: str | None = username
         self._account_id: str | None = account_id
         self._session_id: str | None = None
-        self.__session = requests.Session()
-        self._session()
+        self._session = requests.Session()
+        self._get_session()
 
     def _post(
         self,
@@ -75,9 +72,9 @@ class Dexcom:
         :param params: `dict` to send in the query string of the post request
         :param json: JSON to send in the body of the post request
         """
-        response = self.__session.post(
-            f"{self._base_url}/{endpoint}",
-            headers={"Accept-Encoding": "application/json"},
+        response = self._session.post(
+            f"{self._base_url}{endpoint}",
+            headers=HEADERS,
             params=params,
             json={} if json is None else json,
         )
@@ -128,6 +125,13 @@ class Dexcom:
                 _LOGGER.debug("%s: %s", code, message)
         return error
 
+    def _validate_user_ids(self, account_id: str | None, username: str | None) -> None:
+        user_ids = sum(user_id is not None for user_id in [account_id, username])
+        if user_ids == 0:
+            raise ArgumentError(ArgumentErrorEnum.NONE_USER_ID_PROVIDED)
+        if user_ids != 1:
+            raise ArgumentError(ArgumentErrorEnum.TOO_MANY_USER_ID_PROVIDED)
+
     def _validate_session_id(self) -> None:
         """Validate session ID."""
         if any(
@@ -164,20 +168,57 @@ class Dexcom:
         if self._account_id == DEFAULT_UUID:
             raise ArgumentError(ArgumentErrorEnum.ACCOUNT_ID_DEFAULT)
 
+    def _validate_minutes_max_count(self, minutes: int, max_count: int) -> None:
+        if not isinstance(minutes, int) or any([minutes < 0, minutes > MAX_MINUTES]):
+            raise ArgumentError(ArgumentErrorEnum.MINUTES_INVALID)
+        if not isinstance(max_count, int) or any(
+            [max_count < 0, max_count > MAX_MAX_COUNT],
+        ):
+            raise ArgumentError(ArgumentErrorEnum.MAX_COUNT_INVALID)
+
+    @property
+    def _authenticate_endpoint_arguments(self) -> dict[str, Any]:
+        return {
+            "endpoint": DEXCOM_AUTHENTICATE_ENDPOINT,
+            "json": {
+                "accountName": self._username,
+                "password": self._password,
+                "applicationId": self._application_id,
+            },
+        }
+
+    @property
+    def _login_id_endpoint_arguments(self) -> dict[str, Any]:
+        return {
+            "endpoint": DEXCOM_LOGIN_ID_ENDPOINT,
+            "json": {
+                "accountId": self._account_id,
+                "password": self._password,
+                "applicationId": self._application_id,
+            },
+        }
+
+    def _glucose_readings_endpoint_arguments(
+        self,
+        minutes: int = MAX_MINUTES,
+        max_count: int = MAX_MAX_COUNT,
+    ) -> dict[str, Any]:
+        return {
+            "endpoint": DEXCOM_GLUCOSE_READINGS_ENDPOINT,
+            "params": {
+                "sessionId": self._session_id,
+                "minutes": minutes,
+                "maxCount": max_count,
+            },
+        }
+
     def _get_account_id(self) -> str:
         """Retrieve account ID from the authentication endpoint.
 
         See `pydexcom.const.DEXCOM_AUTHENTICATE_ENDPOINT`.
         """
         _LOGGER.debug("Retrieve account ID from the authentication endpoint")
-        return self._post(
-            DEXCOM_AUTHENTICATE_ENDPOINT,
-            json={
-                "accountName": self._username,
-                "password": self._password,
-                "applicationId": self._application_id,
-            },
-        )
+        return self._post(**self._authenticate_endpoint_arguments)
 
     def _get_session_id(self) -> str:
         """Retrieve session ID from the login endpoint.
@@ -185,16 +226,9 @@ class Dexcom:
         See `pydexcom.const.DEXCOM_LOGIN_ID_ENDPOINT`.
         """
         _LOGGER.debug("Retrieve session ID from the login endpoint")
-        return self._post(
-            DEXCOM_LOGIN_ID_ENDPOINT,
-            json={
-                "accountId": self._account_id,
-                "password": self._password,
-                "applicationId": self._application_id,
-            },
-        )
+        return self._post(**self._login_id_endpoint_arguments)
 
-    def _session(self) -> None:
+    def _get_session(self) -> None:
         """Create Dexcom Share API session."""
         self._validate_password()
 
@@ -215,21 +249,11 @@ class Dexcom:
 
         See `pydexcom.const.DEXCOM_GLUCOSE_READINGS_ENDPOINT`.
         """
-        if not isinstance(minutes, int) or any([minutes < 0, minutes > MAX_MINUTES]):
-            raise ArgumentError(ArgumentErrorEnum.MINUTES_INVALID)
-        if not isinstance(max_count, int) or any(
-            [max_count < 0, max_count > MAX_MAX_COUNT],
-        ):
-            raise ArgumentError(ArgumentErrorEnum.MAX_COUNT_INVALID)
-
         _LOGGER.debug("Retrieve glucose readings from the glucose readings endpoint")
+        self._validate_minutes_max_count(minutes, max_count)
+
         return self._post(
-            DEXCOM_GLUCOSE_READINGS_ENDPOINT,
-            params={
-                "sessionId": self._session_id,
-                "minutes": minutes,
-                "maxCount": max_count,
-            },
+            **self._glucose_readings_endpoint_arguments(minutes, max_count),
         )
 
     def get_glucose_readings(
@@ -250,22 +274,18 @@ class Dexcom:
         try:
             # Requesting glucose reading with DEFAULT_UUID returns non-JSON empty string
             self._validate_session_id()
-
             json_glucose_readings = self._get_glucose_readings(minutes, max_count)
         except SessionError:
             # Attempt to update expired session ID
-            self._session()
-
+            self._get_session()
             json_glucose_readings = self._get_glucose_readings(minutes, max_count)
 
         return [GlucoseReading(json_reading) for json_reading in json_glucose_readings]
 
     def get_latest_glucose_reading(self) -> GlucoseReading | None:
         """Get latest available glucose reading, within the last 24 hours."""
-        glucose_readings = self.get_glucose_readings(max_count=1)
-        return glucose_readings[0] if glucose_readings else None
+        return next(iter(self.get_glucose_readings(max_count=1)), None)
 
     def get_current_glucose_reading(self) -> GlucoseReading | None:
         """Get current available glucose reading, within the last 10 minutes."""
-        glucose_readings = self.get_glucose_readings(minutes=10, max_count=1)
-        return glucose_readings[0] if glucose_readings else None
+        return next(iter(self.get_glucose_readings(minutes=10, max_count=1)), None)
